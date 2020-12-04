49a2c41195cf5490550fbdf0ad4e6e8deaec95a5
Karol Szymanowski <szyma.karol@gmail.com>
2020-12-04 16:09:24 +0100

    ci(#1): configure code coverage and code raports

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,19 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  roots: [
+    '<rootDir>/test',
+    '<rootDir>/src'
+  ],
+  coverageDirectory: './coverage/',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!<rootDir>/src/**/*.interface.ts',
+    '!<rootDir>/src/**/*.mock.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/**/*.test.ts',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/index.ts'
+  ],
 };
