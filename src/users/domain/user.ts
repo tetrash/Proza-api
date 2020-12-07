c1814dd2dca2a42fd8816846501e43473a789a59
Karol Szymanowski <szyma.karol@gmail.com>
2020-12-07 23:51:27 +0100

    feat(#15): add user domain interface and service

---
+++ src/users/domain/user.ts
@@ -0,0 +1,41 @@
+import { IncorrectInputError } from '../../common/errors/errors';
+
+export interface User {
+  id: string;
+  username: string;
+  fullName?: string;
+  role: UserRole | string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum UserRole {
+  admin = 'admin',
+  moderator = 'moderator',
+  user = 'user',
+}
+
+export function newUser(payload: Partial<User>): User {
+  if (!payload.id && typeof payload.id !== 'string') {
+    throw new IncorrectInputError('User is missing id field');
+  }
+
+  if (!payload.username && typeof payload.username !== 'string') {
+    throw new IncorrectInputError('User is missing username field');
+  }
+
+  return {
+    id: payload.id,
+    fullName: payload.fullName,
+    role: payload.role || UserRole.user,
+    username: payload.username,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+}
+
+export interface UserRepository {
+  generateId(): string;
+  getUser(userId: string): Promise<User>;
+  createUser(payload: User): Promise<void>;
+}
