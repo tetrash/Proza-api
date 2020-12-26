0ff2697dd12c356f44a699c6b3387f5de2d6104c
Karol Szymanowski <szyma.karol@gmail.com>
2020-12-26 02:06:01 +0100

    feat(#55): implement post deletion and update

--- dashboard/src/pages/editPost.page.tsx
+++ dashboard/src/pages/editPost.page.tsx
@@ -1,16 +1,21 @@
 import { useParams } from 'react-router-dom';
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_POST, GetPostResult } from '../graphql/post.queries';
 import Loader from '../components/loader';
 import PostForm from '../components/postForm';
+import { UPDATE_POST, UpdatePostInput, UpdatePostResult } from '../graphql/post.mutations';
 
 export default function EditPostPage() {
   const { id } = useParams<{ id: string }>();
-  const { data, loading } = useQuery<GetPostResult, { postId: string }>(GET_POST, { variables: { postId: id } });
+  const { data, loading } = useQuery<GetPostResult, { postId: string }>(GET_POST, {
+    variables: { postId: id },
+    fetchPolicy: 'no-cache',
+  });
+  const [updatePost] = useMutation<UpdatePostResult, UpdatePostInput>(UPDATE_POST);
 
-  const handleSubmit = (e: any) => {
-    console.log(e);
+  const handleSubmit = (payload: { title: string; body: string }): unknown => {
+    return updatePost({ variables: { ...payload, postId: id } });
   };
 
   if (loading) {
