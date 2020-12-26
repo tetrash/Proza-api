d2b75f33d371a7f4e83fe23de12740b5669c05c8
Karol Szymanowski <szyma.karol@gmail.com>
2020-12-26 01:35:55 +0100

    refactor(dashboard): separate gql queries and mutations

--- dashboard/src/graphql/post.mutations.ts
+++ dashboard/src/graphql/post.mutations.ts
@@ -13,39 +13,3 @@ export interface CreatePostResult {
     id: string;
   };
 }
-
-export interface UpdatePostInput {
-  postId: string;
-  title?: string;
-  body?: string;
-}
-
-export const UPDATE_POST = gql`
-  mutation UpdatePost($postId: String!, $title: String, $body: String) {
-    updatePost(post: { body: $body, title: $title, id: $postId }) {
-      title
-      body
-    }
-  }
-`;
-
-export interface UpdatePostResult {
-  updatePost: {
-    title: string;
-    body: string;
-  };
-}
-
-export interface DeletePostInput {
-  postId: string;
-}
-
-export const DELETE_POST = gql`
-  mutation DeletePost($postId: String!) {
-    deletePost(postId: $postId)
-  }
-`;
-
-export interface DeletePostResult {
-  deletePost: boolean;
-}
