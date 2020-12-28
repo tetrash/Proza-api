38ba7ebc8ce1c80546d535f7a61623806a1a50dd
Karol Szymanowski <szyma.karol@gmail.com>
2020-12-28 11:01:09 +0100

    feat(dashboard): add social login button

--- dashboard/src/pages/login.page.tsx
+++ dashboard/src/pages/login.page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button, Grid, Typography } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 import { config } from '../config';
+import SocialLoginButton from '../components/socialLoginButton';
 
 export default function LoginPage() {
   const handleGithubLogin = () => {
@@ -15,26 +17,22 @@ export default function LoginPage() {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
       <Typography variant="h5">Proza admin dashboard - login</Typography>
-      <Button
-        variant="contained"
-        style={{ backgroundColor: '#333333' }}
-        color="primary"
-        size="large"
-        startIcon={<GitHubIcon />}
+      <SocialLoginButton
         onClick={handleGithubLogin}
-      >
-        Login with github
-      </Button>
-      <Button
-        variant="contained"
-        style={{ backgroundColor: '#333333' }}
-        color="primary"
-        size="large"
-        startIcon={<GitHubIcon />}
+        buttonText="Login with github"
+        backgroundColor="#333333"
+        color="white"
+        icon={<GitHubIcon />}
+        width="300px"
+      />
+      <SocialLoginButton
         onClick={handleOidcLogin}
-      >
-        Login with oidc
-      </Button>
+        buttonText="Login with OIDC"
+        backgroundColor="white"
+        color="#333333"
+        icon={<LockOpenIcon />}
+        width="300px"
+      />
     </Grid>
   );
 }
